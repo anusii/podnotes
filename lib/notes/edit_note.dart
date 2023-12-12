6459c1113c81c4ae050470fb8c1d4893c47151b9
Jess Moore <jessclaremoore@gmail.com>
2023-12-13 06:39:27 +1100

    remove unused imports

--- lib/notes/edit_note.dart
+++ lib/notes/edit_note.dart
@@ -32,12 +32,10 @@ import 'package:encrypt/encrypt.dart' as encrypt;
 import 'package:markdown_editor_plus/markdown_editor_plus.dart';
 import 'package:podnotes/constants/colours.dart';
 import 'package:podnotes/common/rest_api/rest_api.dart';
-// import 'package:podnotes/nav_screen.dart';
 import 'package:podnotes/widgets/err_dialogs.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:form_builder_validators/form_builder_validators.dart';
 import 'package:podnotes/widgets/loading_animation.dart';
-//import 'package:simple_markdown_editor/simple_markdown_editor.dart';
 
 class EditNote extends StatefulWidget {
   final String webId;
